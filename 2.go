447d61185e012f386b4b8e62a301eb1992a6f4f1
Jaber <jaberpatwary0@gmail.com>
2025-11-01 23:51:06 +0600

    change

---
+++ 2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(`When earth is sundered, and skies choked black,
+And sleepers serve the seven curses,
+To the hearth there comes a stranger,
+Journeyed far 'neath moon and star.`)
+
+}
